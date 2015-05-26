fedf6a0ec23ff5b1c1ae34d698635fa90f67b87f
Daniel Johnson <danieljohnson2@verizon.net>
2015-05-26 17:03:58 -0400

    I've written a 'four way sprite controller' that tiles 4 sprites together to create a sort of 'super tile'; I'm using it for water and trees to make the graphics look less like blocky.

---
+++ Assets/Scripts/FourWaySpriteController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This controller sets the sprite for the object to
+/// one of four values depending on its location; the idea
+/// is to have an effect like double-sized tiles.
+/// </summary>
+public class FourWaySpriteController : MonoBehaviour
+{
+	public Sprite topLeft;
+	public Sprite topRight;
+	public Sprite bottomLeft;
+	public Sprite bottomRight;
+	private SpriteRenderer spriteRenderer;
+	
+	void Awake ()
+	{
+		this.spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	void Update ()
+	{
+		Sprite newSprite = PickSprite ();
+
+		if (newSprite != null)
+			spriteRenderer.sprite = newSprite;
+	}
+
+	/// <summary>
+	/// This method returns the sprite to use based on
+	/// the current location; the 'top left' sprite is
+	/// used for even numbered x and y positions, and
+	/// we go to 'bottom' and 'right' for odd numbered
+	/// ones. Simple!
+	/// </summary>
+	private Sprite PickSprite ()
+	{
+		Location loc = Location.Of (gameObject);
+		
+		bool hEven = (loc.x % 2) == 0;
+		bool vEven = (loc.y % 2) == 0;
+
+		if (hEven && vEven) {
+			return topLeft;
+		} else if (hEven && !vEven) {
+			return bottomLeft;
+		} else if (!hEven && vEven) {
+			return topRight;
+		} else if (!hEven && !vEven) {
+			return bottomRight;
+		} else {
+			return null;
+		}
+	}
+}
