2cc61358fd29ef2e3e6b7e5ac59db1d0c9fece4c
Daniel Johnson <danieljohnson2@verizon.net>
2015-10-02 17:54:33 -0400

    Fix animation crash; now this works on Linux!
    Now we use animation events to clear the debris cloud, not the hacking
    wait-as-along-as-the-animation.
    
    Also, we're on Unity 5.2 now, which seems to mean more settings for ads.
    Pooh.

--- Assets/Scripts/SlashController.cs
+++ Assets/Scripts/SlashController.cs
@@ -2,14 +2,7 @@
 using System.Collections;
 
 public class SlashController : MonoBehaviour
-{
-	void Start ()
-	{
-		var anim = GetComponent<Animator> ();
-		float len = anim.GetCurrentAnimatorClipInfo(0)[0].clip.length;
-		Invoke ("Unslash", len);
-	}
-
+{	
 	public void Unslash ()
 	{
 		Destroy (gameObject);
