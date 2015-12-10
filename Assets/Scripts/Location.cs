0cae34b8b43c54092f260445f56548a2bd25bd76
Daniel Johnson <danieljohnson2@verizon.net>
2015-12-10 04:13:01 -0500

    Better hashcode
    The hashcode is now optimized for use by LocationMap; it ignores the low
    four bitrs of x and y, which are zero in that case.

--- Assets/Scripts/Location.cs
+++ Assets/Scripts/Location.cs
@@ -11,103 +11,105 @@ using System.IO;
 [Serializable]
 public struct Location : IEquatable<Location>
 {
-	public readonly int x;
-	public readonly int y;
-	public readonly int biasedMapIndex;
+    public readonly int x;
+    public readonly int y;
+    public readonly int biasedMapIndex;
 
-	public int mapIndex {
-		get { return biasedMapIndex - 1; }
-	}
+    public int mapIndex
+    {
+        get { return biasedMapIndex - 1; }
+    }
 
-	public static Location nowhere {
-		get { return default(Location); }
-	}
+    public static Location nowhere
+    {
+        get { return default(Location); }
+    }
 
-	public Location (int x, int y, int mapIndex)
-	{
-		this.x = x;
-		this.y = y;
-		this.biasedMapIndex = mapIndex + 1;
-	}
+    public Location(int x, int y, int mapIndex)
+    {
+        this.x = x;
+        this.y = y;
+        this.biasedMapIndex = mapIndex + 1;
+    }
 
-	public Location (int x, int y, Map map)
-	{
-		this.x = x;
-		this.y = y;
-		this.biasedMapIndex = map.mapIndex + 1;
-	}
+    public Location(int x, int y, Map map)
+    {
+        this.x = x;
+        this.y = y;
+        this.biasedMapIndex = map.mapIndex + 1;
+    }
 
-	/// <summary>
-	/// This method applies a delta to the x and y co-ordinates
-	/// of this location, and returns the updated result.
-	/// </summary>
-	public Location WithOffset (int deltaX, int deltaY)
-	{
-		return new Location (x + deltaX, y + deltaY, mapIndex);
-	}
+    /// <summary>
+    /// This method applies a delta to the x and y co-ordinates
+    /// of this location, and returns the updated result.
+    /// </summary>
+    public Location WithOffset(int deltaX, int deltaY)
+    {
+        return new Location(x + deltaX, y + deltaY, mapIndex);
+    }
 
-	/// <summary>
-	/// This method converts the position to a Unity scene-space
-	/// position. The z co-ordinate is always 0, and the y is
-	/// inverted (typically negatie) because Unity's co-ordinates
-	/// have (0,0) in the bottom left.
-	/// </summary>
-	public Vector3 ToPosition ()
-	{
-		return new Vector3 (x, -y, mapIndex);
-	}
+    /// <summary>
+    /// This method converts the position to a Unity scene-space
+    /// position. The z co-ordinate is always 0, and the y is
+    /// inverted (typically negatie) because Unity's co-ordinates
+    /// have (0,0) in the bottom left.
+    /// </summary>
+    public Vector3 ToPosition()
+    {
+        return new Vector3(x, -y, mapIndex);
+    }
 
-	/// <summary>
-	/// FromPosition() turns a Unity position it its location, reversing
-	/// the effect of ToPosition().
-	/// </summary>
-	public static Location FromPosition (Vector3 position)
-	{
-		return new Location ((int)position.x, -(int)position.y, (int)position.z);
-	}
+    /// <summary>
+    /// FromPosition() turns a Unity position it its location, reversing
+    /// the effect of ToPosition().
+    /// </summary>
+    public static Location FromPosition(Vector3 position)
+    {
+        return new Location((int)position.x, -(int)position.y, (int)position.z);
+    }
 
-	/// <summary>
-	/// This method extracts the location of a game object
-	/// for you.
-	/// </summary>
-	public static Location Of (GameObject gameObject)
-	{
-		return FromPosition (gameObject.transform.localPosition);
-	}
+    /// <summary>
+    /// This method extracts the location of a game object
+    /// for you.
+    /// </summary>
+    public static Location Of(GameObject gameObject)
+    {
+        return FromPosition(gameObject.transform.localPosition);
+    }
 
-	/// <summary>
-	/// Returns a collection containing the locations adjacent
-	/// to this one, north, south, east and west.
-	/// </summary>
-	public IEnumerable<Location> Adjacent ()
-	{
-		var buffer = new Location[4];
-		GetAdjacentInto (buffer);
-		return buffer;
-	}
+    /// <summary>
+    /// Returns a collection containing the locations adjacent
+    /// to this one, north, south, east and west.
+    /// </summary>
+    public IEnumerable<Location> Adjacent()
+    {
+        var buffer = new Location[4];
+        GetAdjacentInto(buffer);
+        return buffer;
+    }
 
-	/// <summary>
-	/// Places the locations adjacent to this one in the buffer
-	/// given, in elements 0-3. 'buffer' should have at least four
-	/// elements. This is equivalent to Adjacent() above but
-	/// does not allocate at all.
-	/// </summary>
-	public void GetAdjacentInto (Location[] buffer)
-	{
-		buffer [0] = new Location (x, y - 1, mapIndex);
-		buffer [1] = new Location (x + 1, y, mapIndex);
-		buffer [2] = new Location (x, y + 1, mapIndex);
-		buffer [3] = new Location (x - 1, y, mapIndex);
-	}
+    /// <summary>
+    /// Places the locations adjacent to this one in the buffer
+    /// given, in elements 0-3. 'buffer' should have at least four
+    /// elements. This is equivalent to Adjacent() above but
+    /// does not allocate at all.
+    /// </summary>
+    public void GetAdjacentInto(Location[] buffer)
+    {
+        buffer[0] = new Location(x, y - 1, mapIndex);
+        buffer[1] = new Location(x + 1, y, mapIndex);
+        buffer[2] = new Location(x, y + 1, mapIndex);
+        buffer[3] = new Location(x - 1, y, mapIndex);
+    }
 
-	/// <summary>
-	/// Returna a textual representation of the location, useful
-	/// for debugging.
-	/// </summary>
-	public override string ToString ()
-	{
-		return string.Format ("({0}, {1} in map #{2})", x, y, mapIndex);
-	}
+    /// <summary>
+    /// Returna a textual representation of the location, useful
+    /// for debugging.
+    /// </summary>
+    public override string ToString()
+    {
+        return string.Format("({0}, {1} in map #{2})", x, y, mapIndex);
+    }
 
     #region Saving
 
@@ -131,36 +133,36 @@ public struct Location : IEquatable<Location>
 
     #region IEquatable implementation
 
-    public static bool operator== (Location left, Location right)
-	{
-		return left.Equals (right);
-	}
-	
-	public static bool operator!= (Location left, Location right)
-	{
-		return !left.Equals (right);
-	}
+    public static bool operator ==(Location left, Location right)
+    {
+        return left.Equals(right);
+    }
 
-	public bool Equals (Location other)
-	{
-		return this.x == other.x && this.y == other.y && this.mapIndex == other.mapIndex;
-	}
+    public static bool operator !=(Location left, Location right)
+    {
+        return !left.Equals(right);
+    }
 
-	public override bool Equals (object other)
-	{
-		return other is Location && Equals ((Location)other);
-	}
+    public bool Equals(Location other)
+    {
+        return this.x == other.x && this.y == other.y && this.mapIndex == other.mapIndex;
+    }
 
-	public override int GetHashCode ()
-	{
+    public override bool Equals(object other)
+    {
+        return other is Location && Equals((Location)other);
+    }
+
+    public override int GetHashCode()
+    {
         unchecked
         {
             int hashCode = mapIndex;
-            hashCode += x * 17;
-            hashCode += y * 31;
+            hashCode += (x >> 4) * 3;
+            hashCode += (y >> 4) * 17;
             return hashCode;
         }
-	}
+    }
 
-	#endregion
+    #endregion
 }
