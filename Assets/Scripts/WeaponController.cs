dc2a9c8dff3c44607980e8c805aa43da8ba1cfce
Applejinx <jinx6568@sover.net>
2016-01-02 09:12:28 -0500

    This should fix the over-fighty mobs.
    Looks like this is what I meant to do: scaling only the overall
    equation means little, so it should’ve scaled only the defender. I
    confess at this hour the way it all fits into the program is mysterious

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -26,12 +26,12 @@ public class WeaponController : ItemController
         {
             int myDamage = damage.Roll();
             int moverDamage = cc.GetAttackDamage();
-			float enemyBrave = (float)(cc.hitPoints)/7f;
+			float enemyBrave = (float)(cc.hitPoints)/3f;
 			//purpose of this is to make the AIs more cowardly the fewer hit points they have.
-			if (enemyBrave < 0.1f) enemyBrave = 0.1f;
+			if (enemyBrave < 0.5f) enemyBrave = 0.5f;
 			//sanity check in case we're given hit points less than zero upon a kill
 			//increasing this value makes them more combative: 10 was very fighty, 1 relatively unfighty
-			return (moverDamage - myDamage) / enemyBrave;
+			return moverDamage - (myDamage / enemyBrave);
         }
 
         return base.GetHeatmapScalingFactor(mover);
