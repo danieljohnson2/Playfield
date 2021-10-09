5cc084183682a4861c9cd089f69c60ff75caec4e
Daniel Johnson <danieljohnson2@fastmail.com>
2021-10-09 08:29:13 -0400

    Move credit-controller script to the Scripts dir

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// CreditsController does little; just provides the handler
+/// for the 'main menu' button.
+/// </summary>
+public class CreditsController : MonoBehaviour
+{
+    public void ReturnToIntro()
+    {
+		SceneManager.LoadScene("Intro");
+    }
+}
